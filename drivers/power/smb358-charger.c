7d5bf9b508bc03684eff1846ea0b1878f57cfc38
Mhmmdfas <fadlyardhians@outlook.com>
2019-09-25 12:22:55 +0800

    smb358-charger: increase current charging up to 1.6mA
    Signed-off-by: fadlyardhians <fadlyardhians@gmail.com>

--- drivers/power/smb358-charger.c
+++ drivers/power/smb358-charger.c
@@ -1241,7 +1241,7 @@ static int get_prop_current_now(struct smb358_charger *chip)
 		} else {
 			pr_debug("No BMS supply registered return 0\n");
 		}
-	return 1000;
+	return 1600;
 }
 
 static int smb358_get_prop_charge_type(struct smb358_charger *chip)
