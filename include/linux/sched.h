6ff6e25c53b167afa494adc22410b4c8fb2ecaaf
fadlyas07 <fadlyardhians@gmail.com>
2020-01-24 03:57:13 +0700

    sched: core: add missing int after commit fd6f0e0
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1338,6 +1338,8 @@ struct task_struct {
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 	unsigned int cpu;	/* current CPU */
 #endif
+	unsigned int wakee_flips;
+	unsigned long wakee_flip_decay_ts;
 	struct task_struct *last_wakee;
 	unsigned long wakee_flips;
 	unsigned long wakee_flip_decay_ts;
